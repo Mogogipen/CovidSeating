8c2b49612936c2c2a184ca551583efe98e9b182a
Nickolas Reimschiissel <ryming.chizel@gmail.com>
2020-12-31 20:54:11 -0700

    Commandline finished for single-sitters
    +Command line version for individual seating is finished.

--- src/seating/SeatingChart.java
+++ src/seating/SeatingChart.java
@@ -26,7 +26,9 @@ public class SeatingChart {
 	}
 	
 	public boolean seatAvailable(int row, int col) {
-		return (seats[row][col] == State.OPEN);
+		if (row > 0 && row < seats.length && col > 0 && col < seats[0].length)
+			return (seats[row][col] == State.OPEN);
+		return false;
 	}
 	
 	public boolean takeSeat(int row, int col) {
@@ -39,20 +41,65 @@ public class SeatingChart {
 	}
 	
 	private void addPadding(int row, int col) {
-		State[] pos = new State[4];
-		pos[0] = seats[row-1][col];
-		pos[1] = seats[row+1][col];
-		pos[2] = seats[row][col-1];
-		pos[3] = seats[row][col+1];
-		for (int i = 0; i < 4; i++) {
-			if (pos[0] != )
+		pad(row-1, col);
+		pad(row+1, col);
+		pad(row, col+1);
+		pad(row, col-1);
+	}
+	private void pad(int r, int c) {
+		try {
+			if (seats[r][c] == State.OPEN)
+				seats[r][c] = State.PADDING;
+		} catch (Exception e) {
+			
 		}
+	}
+	
+	public String toString() {
+		String result = "";
+		
+		// Column numbers
+		result += "   ";
+		for (int i = 0; i < seats[0].length; i++) {
+			result += " " + (i+1);
+		}
+		result += "\n";
+		
+		// Rows with a number at the start
+		for (int i = 0; i < seats.length; i++) {
+			result += ((i+1) + " |");
+			for (int j = 0; j < seats[0].length; j++) {
+				result += " ";
+				State s = seats[i][j];
+				if (s == State.OPEN)
+					result += "O";
+				else if (s == State.TAKEN)
+					result += "X";
+				else
+					result += ".";
+			}
+			result += " |\n";
+		}
+		return result;
 	}
 	
 	public static void main(String args[]) {
 		int rows = getUserInteger("How many rows are there in the theater?");
 		int cols = getUserInteger("How many columns are there in the theater?");
 		SeatingChart s1 = new SeatingChart(rows, cols);
+		System.out.println(s1);
+		
+		while(true) {
+			boolean seatTaken = false;
+			while (!seatTaken) {
+				int row = getUserInteger("Which row do you want to sit at?")-1;
+				int col = getUserInteger("Which seat do you want in that row? (Column)")-1;
+				seatTaken = s1.takeSeat(row, col);
+				if (!seatTaken)
+					System.out.println("Sorry, that seat is either invalid, or unavailable");
+			}
+			System.out.println(s1);
+		}
 	}
 	
 	public static int getUserInteger(String request) {
@@ -61,7 +108,11 @@ public class SeatingChart {
 		boolean proceed = false;
 		while (!proceed) {
 			System.out.println(request);
-			x = in.nextInt();
+			try {
+				x = in.nextInt();
+			} catch (Exception e) {
+				break;
+			}
 			boolean valid = false;
 			boolean confirmed = false;
 			while (!valid) {
